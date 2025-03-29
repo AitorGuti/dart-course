1b0fa39dd05ad0424103988c664d4cdba29f8b1c
Theguiderdawg <gudiance007@gmail.com>
2025-03-29 06:20:28 +0000

    addded to variab.dart

---
+++ learningapp/bin/variable.dart
@@ -0,0 +1,27 @@
+void main() {
+
+  int num1 = 5;
+  double num2 = 3.5;
+  bool isTrue = true;
+
+  print((num1 + num2) is int);
+
+  print((num1 + num2).runtimeType);
+
+  print ((num1 + num2));
+  
+  var str1 = 'helnig';
+
+  // String interpolation
+  
+  print('The contexnt of $str1 is a string');
+
+  
+
+
+
+
+
+
+
+}
